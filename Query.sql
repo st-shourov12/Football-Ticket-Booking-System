6d6d00b05cf868f63a6c3d3fc90ec9b8e34f280a
st-shourov12 <shourovsc38@gmail.com>
2026-07-07 01:26:09 +0600

    solving query 4

--- Query.sql
+++ Query.sql
@@ -110,18 +110,23 @@ WHERE payment_status IS NULL ;
 -- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
 -- =========================================================================
 
-
+SELECT b.booking_id, u.full_name, m.fixture, total_cost FROM Bookings AS b
+JOIN Users AS u ON u.user_id = b.user_id 
+JOIN Matches AS m ON b.match_id = m.match_id ;
 
 -- =========================================================================
 -- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
 -- =========================================================================
 
-
+-- SELECT u.user_id,	u.full_name,	booking_id FROM Users AS u 
+-- LEFT JOIN Bookings AS b ON b.user_id = u.user_id ;
 
 -- =========================================================================
 -- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
 -- =========================================================================
 
+-- SELECT booking_id, match_id, total_cost FROM Bookings 
+-- WHERE total_cost > (SELECT AVG(total_cost) FROM Bookings) ;
 
 
 -- =========================================================================
