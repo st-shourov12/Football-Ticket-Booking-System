823a81d6c8aa909808d14049c658d47fb652d201
st-shourov12 <shourovsc38@gmail.com>
2026-07-07 00:47:39 +0600

    spolving query 2

--- Query.sql
+++ Query.sql
@@ -90,5 +90,8 @@ SELECT match_id, fixture, base_ticket_price FROM MATCHES
 WHERE tournament_category = 'Champions League' AND match_status = 'Available'
 
 -- =========================================================================
--- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 -- =========================================================================
+
+SELECT user_id , full_name, email FROM Users 
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%'
